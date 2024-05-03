d06eb82ecd889ce441d82b530ce2b0ad991f5608
fan1300 <fan1300@naver.com>
2024-05-04 01:34:16 +0900

    병합6

--- planpeak/settings.py
+++ planpeak/settings.py
@@ -57,8 +57,8 @@ import os
 
 TEMPLATES = [
     {
-         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-         'DIRS': [os.path.join(BASE_DIR, 'mail', 'templates')],
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'mail', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
