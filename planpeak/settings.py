d876b3b8a00a183b24aed26becf1b73eb82a06d5
fan1300 <fan1300@naver.com>
2024-05-04 02:10:57 +0900

    smtp password 제거

--- planpeak/settings.py
+++ planpeak/settings.py
@@ -130,6 +130,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT =587
 EMAIL_HOST_USER = 'planpeak001@gmail.com'
-EMAIL_HOST_PASSWORD = 'drqf ppzd ptxf xskh'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
